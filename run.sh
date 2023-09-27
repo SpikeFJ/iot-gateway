9eec6753b1b4b074de998e8086f23efbbb624ab3
fengji <261325104@qq.com>
2023-09-27 16:58:09 +0800

    添加服务端自动部署

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ if [ -n "$pid" ]
   kill -9 $pid
 fi
 
-nohup java  -Dcom.sun.management.jmxremote -Djava.net.preferIPv4Stack=true -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false  -Djava.rmi.server.hostname=144.202.100.130 -Dcom.sun.management.jmxremote.port=9093  -jar -Xms512m -Xmx512m -Xss512k iot-gateway.jar >> /dev/null 2>&1 &
+nohup java  -jar -Xms512m -Xmx512m -Xss512k iot-gateway.jar >> /dev/null 2>&1 &
